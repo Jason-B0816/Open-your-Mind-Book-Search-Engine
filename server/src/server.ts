2643dfd48885a51e909bfd67b2be79313c91f7e3
Jason-B0816git config --global user.name Jason-B0816 <jbrooks200800@gmail.com>
2025-05-18 17:50:08 -0500

    test new code, removed import path, fileURLtoPath, remove const_filename

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,15 @@
 import express from 'express';
-import path from 'node:path';
+// import path from 'node:path';
 import type { Request, Response } from 'express';
 import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth.js';
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3001;
   
 const server = new ApolloServer({
